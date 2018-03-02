6b2b1157bd39a7ae6601a8d018c9f1c7f63c1b4c
Ștefan Istrate <stefan.istrate@gmail.com>
2018-03-02 19:00:45 +0000

    Build a new implementation based on tf.layers.

---
+++ cnn/main.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from model import cnn_model_fn
+
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_integer(
+        'training_steps', 20000,
+        'Number of training steps.')
+tf.app.flags.DEFINE_integer(
+        'training_batch_size', 100,
+        'Number of training steps.')
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+
+def main(unused_argv):
+    # Load input data and split it into train & eval.
+    df = pd.read_csv('data/train.csv', dtype=np.uint8)
+    train_df = df.sample(frac=0.9, random_state=777)
+    train_data = train_df.iloc[:, 1:].values.astype(np.float32)
+    train_labels = train_df.iloc[:, 0].values.astype(np.int32)
+    eval_df = df.drop(train_df.index)
+    eval_data = eval_df.iloc[:, 1:].values.astype(np.float32)
+    eval_labels = eval_df.iloc[:, 0].values.astype(np.int32)
+
+    # Construct file paths.
+    unique_name = (
+            ('CNN_GradientDescent'
+             '_TrainingSteps_%d'
+             '_BatchSize_%d'
+             '_Conv_%dx%d_%d'
+             '_Pool_%dx%d_%d'
+             '_Conv_%dx%d_%d'
+             '_Pool_%dx%d_%d'
+             '_Dense_%d'
+             '_DropoutRate_%f'
+             '_LearningRate_%f')
+            % (FLAGS.training_steps,
+               FLAGS.training_batch_size,
+               FLAGS.conv1_kernel_size, FLAGS.conv1_kernel_size,
+               FLAGS.conv1_filters,
+               FLAGS.pool1_pool_size, FLAGS.pool1_pool_size,
+               FLAGS.pool1_strides,
+               FLAGS.conv2_kernel_size, FLAGS.conv2_kernel_size,
+               FLAGS.conv2_filters,
+               FLAGS.pool2_pool_size, FLAGS.pool2_pool_size,
+               FLAGS.pool2_strides,
+               FLAGS.dense_units,
+               FLAGS.dropout_rate,
+               FLAGS.learning_rate)
+    )
+    model_dir = 'models/%s' % unique_name
+    output_txt = 'outputs/%s.txt' % unique_name
+
+    # Create the estimator.
+    classifier = tf.estimator.Estimator(model_fn=cnn_model_fn,
+                                        model_dir=model_dir)
+
+    # Train the model.
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+            x={'x': train_data},
+            y=train_labels,
+            batch_size=FLAGS.training_batch_size,
+            num_epochs=None,
+            shuffle=True)
+    classifier.train(input_fn=train_input_fn,
+                     steps=FLAGS.training_steps)
+
+    # Evaluate the model.
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+            x={'x': eval_data},
+            y=eval_labels,
+            num_epochs=1,
+            shuffle=False)
+    eval_results = classifier.evaluate(input_fn=eval_input_fn)
+
+    # Predict with the model.
+    predict_data = (pd.read_csv('data/test.csv', dtype=np.uint8)
+                    .values.astype(np.float32))
+    predict_input_fn = tf.estimator.inputs.numpy_input_fn(
+            x={'x': predict_data},
+            num_epochs=1,
+            shuffle=False)
+    predict_results = classifier.predict(input_fn=predict_input_fn)
+
+    # Write predictions.
+    predict_classes = [result['class'] for result in predict_results]
+    output_df = pd.DataFrame({'Label' : pd.Series(predict_classes)})
+    output_df.index += 1
+    output_df.index.name = 'ImageId'
+    output_df.to_csv(output_txt)
+
+
+if __name__ == '__main__':
+    tf.app.run()
---
+++ cnn/model.py
@@ -0,0 +1,114 @@
+import tensorflow as tf
+
+
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_integer(
+        'conv1_filters', 32,
+        'Number of output filters for conv1 layer.')
+tf.app.flags.DEFINE_integer(
+        'conv1_kernel_size', 5,
+        'Kernel size for conv1 layer.')
+tf.app.flags.DEFINE_integer(
+        'pool1_pool_size', 2,
+        'Max pooling filter size for pool1 layer.')
+tf.app.flags.DEFINE_integer(
+        'pool1_strides', 2,
+        'Stride size for pool1 layer.')
+tf.app.flags.DEFINE_integer(
+        'conv2_filters', 64,
+        'Number of output filters for conv2 layer.')
+tf.app.flags.DEFINE_integer(
+        'conv2_kernel_size', 5,
+        'Kernel size for conv2 layer.')
+tf.app.flags.DEFINE_integer(
+        'pool2_pool_size', 2,
+        'Max pooling filter size for pool2 layer.')
+tf.app.flags.DEFINE_integer(
+        'pool2_strides', 2,
+        'Stride size for pool2 layer.')
+tf.app.flags.DEFINE_integer(
+        'dense_units', 1024,
+        'Number of output units for the dense layer.')
+tf.app.flags.DEFINE_float(
+        'dropout_rate', 0.4,
+        'Dropout rate to apply to the dense layer.')
+tf.app.flags.DEFINE_float(
+        'learning_rate', 0.001,
+        'Learning rate for the optimizer.')
+
+
+def cnn_model_fn(features, labels, mode):
+    # Input layer. Reshape pixel features to
+    # [batch_size, image_width, image_height, num_color_channels].
+    input_layer = tf.reshape(features['x'], [-1, 28, 28, 1])
+
+    # Convolutional layer #1.
+    conv1 = tf.layers.conv2d(inputs=input_layer,
+                             filters=FLAGS.conv1_filters,
+                             kernel_size=FLAGS.conv1_kernel_size,
+                             padding='same',
+                             activation=tf.nn.relu)
+
+    # Pooling layer #1.
+    pool1 = tf.layers.max_pooling2d(inputs=conv1,
+                                    pool_size=FLAGS.pool1_pool_size,
+                                    strides=FLAGS.pool1_strides)
+
+    # Convolutional layer #2.
+    conv2 = tf.layers.conv2d(inputs=pool1,
+                             filters=FLAGS.conv2_filters,
+                             kernel_size=FLAGS.conv2_kernel_size,
+                             padding='same',
+                             activation=tf.nn.relu)
+
+    # Pooling layer #2.
+    pool2 = tf.layers.max_pooling2d(inputs=conv2,
+                                    pool_size=FLAGS.pool2_pool_size,
+                                    strides=FLAGS.pool2_strides)
+
+    # Flatten pooling layer #2.
+    num_reduced_dim = int(28 / FLAGS.pool1_strides / FLAGS.pool2_strides)
+    pool2_flat = tf.reshape(
+            pool2,
+            [-1, num_reduced_dim * num_reduced_dim * FLAGS.conv2_filters])
+
+    # Dense layer.
+    dense = tf.layers.dense(inputs=pool2_flat,
+                            units=FLAGS.dense_units,
+                            activation=tf.nn.relu)
+    dropout = tf.layers.dropout(inputs=dense,
+                                rate=FLAGS.dropout_rate,
+                                training=(mode == tf.estimator.ModeKeys.TRAIN))
+
+    # Logits layer.
+    logits = tf.layers.dense(inputs=dropout, units=10)
+
+    # Generate predictions (for PREDICT and EVAL mode). Also add
+    # `softmax_tensor` to the graph.
+    predictions = {
+        'class': tf.argmax(input=logits, axis=1),
+        'probabilities': tf.nn.softmax(logits, name='softmax_tensor')
+    }
+    if mode == tf.estimator.ModeKeys.PREDICT:
+      return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+    # Calculate loss (for both TRAIN and EVAL modes).
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+
+    # Configure the training op (for TRAIN mode).
+    if mode == tf.estimator.ModeKeys.TRAIN:
+      optimizer = tf.train.GradientDescentOptimizer(
+              learning_rate=FLAGS.learning_rate)
+      train_op = optimizer.minimize(loss=loss,
+                                    global_step=tf.train.get_global_step())
+      return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+    # Add evaluation metrics (for EVAL mode).
+    eval_metric_ops = {
+        'accuracy': tf.metrics.accuracy(labels=labels,
+                                        predictions=predictions['class'])
+    }
+    return tf.estimator.EstimatorSpec(mode=mode,
+                                      loss=loss,
+                                      eval_metric_ops=eval_metric_ops)
