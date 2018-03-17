114a0d2667ac9f85b1ae8fa70b94bb52a5665010
Ștefan Istrate <stefan.istrate@gmail.com>
2018-03-17 11:26:20 +0000

    Tweak grid search to reduce the search space.

--- cnn/grid_search.py
+++ cnn/grid_search.py
@@ -4,29 +4,29 @@ import itertools
 import subprocess
 
 
-num_runs = 4
+num_runs = 1
 training_steps = 5000
 
-batch_sizes = [25, 50, 100, 200]
-conv1_filters = [16, 32, 64]
-conv1_kernel_sizes = [3, 5, 7]
-pool1_pool_sizes = [2, 3, 4]
-pool1_strides = [2, 3]
-conv2_filters = [16, 32, 64]
-conv2_kernel_sizes = [3, 5, 7]
-pool2_pool_sizes = [2, 3, 4]
-pool2_strides = [2, 3]
-dense_units = [128, 256, 512, 1024]
-dropout_rates = [0.1, 0.3, 0.4, 0.5, 0.7]
-optimizers = ['Adagrad', 'Adam', 'GradientDescent']
-learning_rates = [1, 0.5, 0.25, 0.1, 0.05, 0.025, 0.01, 0.005]
+batch_sizes = [100]
+conv1_filters = [32]
+conv1_kernel_sizes = [5]
+pool1_pool_sizes = [2]
+pool1_strides = [2]
+conv2_filters = [32, 64]
+conv2_kernel_sizes = [5]
+pool2_pool_sizes = [2]
+pool2_strides = [2]
+dense_units = [512, 1024]
+dropout_rates = [0.2, 0.4, 0.6]
+optimizers = ['Adagrad', 'Adam', 'Ftrl', 'GradientDescent', 'RMSProp']
+learning_rates = [0.25, 0.1, 0.05, 0.025]
 
 all_configs = (batch_sizes, conv1_filters, conv1_kernel_sizes, pool1_pool_sizes,
                pool1_strides, conv2_filters, conv2_kernel_sizes,
                pool2_pool_sizes, pool2_strides, dense_units, dropout_rates,
                optimizers, learning_rates)
 default = (100, 32, 5, 2, 2, 64, 5, 2, 2, 1024, 0.4, 'GradientDescent', 0.1)
-max_diffs = 2
+max_diffs = 3
 
 
 if __name__ == '__main__':
@@ -36,6 +36,7 @@ if __name__ == '__main__':
         if num_diffs <= max_diffs:
             configs.append(config)
 
+    print('>>> NUM RUNS: %d <<<' % num_runs)
     print('>>> NUM CONFIGS: %d <<<' % len(configs))
     for i in range(num_runs):
         print('>>> RUN #%d <<<' % (i + 1))
