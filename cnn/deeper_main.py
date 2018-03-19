Fix data normalization issues in EVAL & PREDICT mode, and increase the number of units in the fully connected layer of the deeper model.

cnn/deeper_main.py
@@ -21,7 +21,9 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 def normalize_data(data, mean, std):
     with np.errstate(divide='ignore', invalid='ignore'):
-        return np.nan_to_num((data - mean) / std)
+        data = (data - mean) / std
+        data[~np.isfinite(data)] = 0.0
+        return data
 
 
 def main(unused_argv):

cnn/deeper_model.py
@@ -38,9 +38,9 @@ def model_fn(features, labels, mode):
     conv_group2 = _build_conv_group(conv_group1, 64, 3, mode)
 
     # Dense layer.
-    flatten = tf.reshape(conv_group2, [-1, 7 * 7 * 64])
-    dense = tf.layers.dense(inputs=flatten,
-                            units=256,
+    flattened = tf.reshape(conv_group2, [-1, 7 * 7 * 64])
+    dense = tf.layers.dense(inputs=flattened,
+                            units=1024,
                             activation=tf.nn.relu)
     dropout = tf.layers.dropout(inputs=dense,
                                 rate=0.5,